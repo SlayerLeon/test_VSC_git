e7e78f2638debe665530282289b6509ee0581051
SlayerLeon <lev.volkov.98@gmail.com>
2023-07-23 03:10:45 +0500

    Add new functions

--- hw.py
+++ hw.py
@@ -1,2 +1,11 @@
 name = str(input("How you name: \n"))
 print(f"Hi, {name}")
+try:
+    quest = str(input(f"My name is {name} \nYou like eat fish?"))
+except Exception as e:
+    print(f"Error type data: {e}")
+if quest != ("yes" | "y" | "n" | "no"):
+    print("Fail")
+else:
+    print("Nice")
+
