a738d7dbde0eade2412a51aa2e68fdfb04026b81
SlayerLeon <lev.volkov.98@gmail.com>
2023-07-28 19:06:42 +0500

    Delete file hw.py | Modified file test280723.py | Add file README.md | THIS ALL FOR TEST

---
+++ README.md
@@ -0,0 +1,5 @@
+# This BIG TEST of Github's my head brain
+
+TEST
+TEST
+TEST
---
+++ test280723.py
@@ -0,0 +1,4 @@
+a = int(input("age: \n"))
+print(f"she age is: {a}"))
+b = str(input("what is your branch: \n"))
+print(f"Your branch is {b}")
